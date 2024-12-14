e3606810696ee57cbc9f86ddc9e550b9e51bdbb1
rakshit-saini <rakshitsaini2004@gmail.com>
2024-12-14 16:31:48 +0530

    https://github.com/rakshit-saini/FULL-STACK-TASK-MANAGEMENT-APP

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -4,7 +4,7 @@ export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
       dbName: "MERN_STACK_TASK_MANAGEMENT",
-    })
+    }) 
     .then(() => {
       console.log("Connected to database!");
     })
