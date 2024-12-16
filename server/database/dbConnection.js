40384e7749ced043631e62fb0db1bcc75a2ca6fd
rakshit-saini <rakshitsaini2004@gmail.com>
2024-12-16 12:43:01 +0530

    third change

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "MERN_STACK_TASK_MANAGEMENT",
+      
     }) 
     .then(() => {
         console.log("connected to db")
